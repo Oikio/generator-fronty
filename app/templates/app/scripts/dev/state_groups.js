aa0a8f93afaea7991933ecc54733978ad004f3c7
Stas <oikiostas@gmail.com>
2014-03-24 15:17:49 +0600

    first configs attempts

--- app/templates/app/scripts/dev/state_groups.js
+++
@@ -1,8 +0,0 @@
-// Place for example data groups
-'use strict';
-
-gState.partials = {};
-gState.partials.header = gBlocks.header;
-gState.partials.footer = gBlocks.footer;
-
-gState.main = Object.create(gState.partials);
